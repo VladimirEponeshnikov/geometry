4f67c98d39fb170ebf44bedbeefcfe58d9128a15
VladimirEponeshnikov <transmuker.vova@gmail.com>
2019-05-06 10:38:56 +0700

    main.cpp bugfix

--- main.cpp
+++ main.cpp
@@ -4,13 +4,13 @@
 using namespace std;
 
 int main()
-}
+{
     const char circle[7] = {'c', 'i', 'r', 'c', 'l', 'e'};
     const char triangle[9] = {'t', 'r', 'i', 'a', 'n', 'g', 'l', 'e'};  
-    
+    int count;
     do {
         cout << "Enter number of figures" << endl;
-        cin >> int count;
+        cin >> count;
         if (count < 1) cout << "Error! Try again" << endl;
     } while (count < 1);
     char str[count][9];
@@ -18,7 +18,7 @@ int main()
     for (int i = 0; i < count; i++) {
         do {
             cout << "Enter name of figure" << endl;
-            gets(str[i]);
+	    cin.getline(str[i], 9);
             if ((strcmp(str[i], circle) != 0) and (strcmp(str[i], triangle) != 0))
                 cout << "Error! Invalid name" << endl; 
         } while ((strcmp(str[i], circle) != 0) and (strcmp(str[i], triangle) != 0));
