701a2dbd8d3f0b42e32b1fb3326bcba4642ab092
VladimirEponeshnikov <transmuker.vova@gmail.com>
2019-05-06 10:14:06 +0700

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream> 
+#include <string.h>
+
+using namespace std;
+
+int main()
+}
+    const char circle[7] = {'c', 'i', 'r', 'c', 'l', 'e'};
+    const char triangle[9] = {'t', 'r', 'i', 'a', 'n', 'g', 'l', 'e'};  
+    
+    do {
+        cout << "Enter number of figures" << endl;
+        cin >> int count;
+        if (count < 1) cout << "Error! Try again" << endl;
+    } while (count < 1);
+    char str[count][9];
+
+    for (int i = 0; i < count; i++) {
+        do {
+            cout << "Enter name of figure" << endl;
+            gets(str[i]);
+            if ((strcmp(str[i], circle) != 0) and (strcmp(str[i], triangle) != 0))
+                cout << "Error! Invalid name" << endl; 
+        } while ((strcmp(str[i], circle) != 0) and (strcmp(str[i], triangle) != 0));
+    }
+
+    for (int i = 0; i < count; i++) {
+        cout << (i + 1) << ".";
+        puts(str[i]);
+        cout << endl;
+    }
+   
+    cin.get();
+    return 0;
+}
